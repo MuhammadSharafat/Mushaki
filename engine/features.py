44ef094dfd43c50b06597a5a4e1ae420ee5a946b
MuhammadSharafat <muhammadsharafatalam@gmail.com>
2026-09-02 07:38:12 +0600

    sound & voice text

---
+++ engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+# playing assistant sound function
+def playassistantSound():
+    music_dir = "D:\\Mushaki\\www\\assets\\audio\\start_sound.mp3"
+    playsound(music_dir)
