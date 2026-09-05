3d1feaade3d4e22ee91cf2ed395c43fc87856cba
MuhammadSharafat <muhammadsharafatalam@gmail.com>
2026-09-05 06:40:06 +0600

    Face reorganization

--- main.py
+++ main.py
@@ -3,11 +3,28 @@ import os
 import eel
 from engine.features import *
 from engine.command import *
-
+from engine.auth import recoganize
 def start():
     eel.init("www")
 
+    
+
     playassistantSound()
+    @eel.expose
+    def init():
+        subprocess.call([r'device.bat'])
+        eel.hideLoader()
+        speak("Ready for Face Authentication")
+        flag = recoganize.AuthenticateFace()
+        if flag == 1:
+            eel.hideFaceAuth()
+            speak("Face Authentication Successful")
+            eel.hideFaceAuthSuccess()
+            speak("Hello, Welcome Sir, How can i Help You")
+            eel.hideStart()
+            playassistantSound()
+        else:
+            speak("Face Authentication Fail")
 
     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
--- www/main.js
+++ www/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    eel.init()()
      
     $('.text').textillate({
         loop: true,
