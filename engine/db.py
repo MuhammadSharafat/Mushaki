bb5013bcc0ebbf32f48d1ca2717b8aee15e77057
MuhammadSharafat <muhammadsharafatalam@gmail.com>
2026-09-08 03:34:04 +0600

    Adding Settings Button

--- engine/db.py
+++ engine/db.py
@@ -20,7 +20,7 @@ cursor = con.cursor()
 # con.commit()
 
 # Create a table with the desired columns
-# cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+# cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL, address VARCHAR(255) NULL)''')
 
 
 # Specify the column indices you want to import (0-based index)
@@ -39,10 +39,18 @@ cursor = con.cursor()
 # con.close()
 
 
-
 # query = 'Shyîb Cox Frn'
 # query = query.strip().lower()
 
 # cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
 # results = cursor.fetchall()
 # print(results[0][0])
+
+
+
+# Adding personal info table
+# query = "CREATE TABLE IF NOT EXISTS info(name VARCHAR(100), designation VARCHAR(50),mobileno VARCHAR(40), email VARCHAR(200), city VARCHAR(300))"
+# cursor.execute(query)
+
+# Add Column in contacts table
+# cursor.execute("ALTER TABLE contacts ADD COLUMN address VARCHAR(255)")
